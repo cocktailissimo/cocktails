c8d5ce7f88000488309259f0b10cecbdeba6a4b0
michele goglio <cocktailissimo@gmail.com>
2021-04-18 23:25:29 -0400

    expand

--- src/components/CocktailGrid.js
+++ src/components/CocktailGrid.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
 
 export default function CocktailGrid({cocktailResults}) {
-   const [isExpanded, setIsIExpanded] = useState(false);
-   const [isIndex, setIsIndex] = useState();
-   let isFullDescription = '';
+  const [isExpanded, setIsIExpanded] = useState(false);
+  const [isIndex, setIsIndex] = useState();
 
-  function showFull(index) {
-    setIsIExpanded(!isExpanded);
-    setIsIndex(index);
-    
-  }
+ function showFull(index) {
+   setIsIExpanded(!isExpanded);
+   setIsIndex(index);
+   
+ }
+
   return (
     <>
     <ul aria-live="polite" className="list">
         {cocktailResults.map((cocktail, index) => {
         const { nom, remarques, id } = cocktail;
-        console.log(isExpanded, index, isIndex);
         return (
             <li key={index} className="character">
               <button onClick={() => showFull(index)}>
                 <div><strong>{nom}</strong></div>
               </button>
-                <span className={isExpanded && (index === isIndex) ? 'expanded' : 'collapsed'}>
+              <span className={isExpanded && (index === isIndex) ? 'expanded' : 'collapsed'}>
                   <div><strong>{id}</strong></div>
                   <div>{remarques}</div>
                   {cocktail.recette.map((recettecocktail) => {
