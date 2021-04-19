0ecf2948c62108a6a0d7aea811e9f03fd10739aa
michele goglio <cocktailissimo@gmail.com>
2021-04-18 22:55:51 -0400

    add expand

--- src/components/CocktailGrid.js
+++ src/components/CocktailGrid.js
@@ -1,40 +1,39 @@
 import React, { useState } from 'react';
 
 export default function CocktailGrid({cocktailResults}) {
+   const [isExpanded, setIsIExpanded] = useState(false);
    const [isIndex, setIsIndex] = useState();
    let isFullDescription = '';
 
   function showFull(index) {
-    setIsIndex(!isIndex);
-    isFullDescription = index === isIndex;
+    setIsIExpanded(!isExpanded);
+    setIsIndex(index);
+    
   }
   return (
     <>
     <ul aria-live="polite" className="list">
         {cocktailResults.map((cocktail, index) => {
         const { nom, remarques, id } = cocktail;
+        console.log(isExpanded, index, isIndex);
         return (
             <li key={index} className="character">
               <button onClick={() => showFull(index)}>
                 <div><strong>{nom}</strong></div>
               </button>
-                {
-                  isFullDescription && isIndex === index && (
-                    <>
-                      <div><strong>{id}</strong></div>
-                      <div>{remarques}</div>
-                      {cocktail.recette.map((recettecocktail) => {
-                          return (
-                              <div key={recettecocktail.etape1}>
-                                <div>{recettecocktail.etape1}</div>
-                                <div>{recettecocktail.etape2}</div>
-                                <div>{recettecocktail.etape3}</div>
-                              </div> 
-                            );
-                      })}
-                    </>
-                  )  
-                }
+                <span className={isExpanded && (index === isIndex) ? 'expanded' : 'collapsed'}>
+                  <div><strong>{id}</strong></div>
+                  <div>{remarques}</div>
+                  {cocktail.recette.map((recettecocktail) => {
+                      return (
+                        <div key={recettecocktail.etape1}>
+                          <div>{recettecocktail.etape1}</div>
+                          <div>{recettecocktail.etape2}</div>
+                          <div>{recettecocktail.etape3}</div>
+                        </div> 
+                      );
+                  })}
+                </span>
             </li>
         )
         }).slice(0,4).sort().reverse()}
